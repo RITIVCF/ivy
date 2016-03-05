e34c1f71ea5336214964fa1dd1fa120898204ae1
Michael Plante <michaelplante2@gmail.com>
2016-03-05 12:39:19 -0500

    Added public site

--- ivy/settings.py
+++ ivy/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ivyManager',
+    'publicSite',
 ]
 
 MIDDLEWARE_CLASSES = [
