e299bc8ea8fad56d3cb65aa7fdd40bb58a6f76b6
Alex Eckard <awe6013@rit.edu>
2016-08-31 23:01:21 -0400

    trying to resolve conflict from stash

--- server/configs/accounts.js
+++ server/configs/accounts.js
@@ -3,6 +3,9 @@ import { Accounts } from 'meteor/accounts-base';
 //export default () => {
 
 // For environment variable when testing locally
+if(Meteor.settings.MAIL_URL){
+    process.env.MAIL_URL = Meteor.settings.MAIL_URL;
+}
 
   Accounts.onCreateUser(function(options, user) {
     //let contact = this.getContact(redundantEmail)
