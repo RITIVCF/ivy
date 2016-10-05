8b1dfb4b5f5c49c2fb5126860719731f29bb2c4e
awe6013 <awe6013@rit.edu>
2016-10-05 10:27:09 -0400

    Manual Statuses Feature Working.

--- client/contact/ContactSingle.jsx
+++ client/contact/ContactSingle.jsx
@@ -4,7 +4,8 @@ export default class ContactSingle extends Component {
   constructor(){
     super();
     this.state = {
-      ran: false
+      ran: false,
+      opened: false
     };
   }
 
@@ -13,8 +14,21 @@ export default class ContactSingle extends Component {
     FlowRouter.go("/tickets/"+this.props.contact.ticket);
   }
 
+  changeStatus(){
+    Meteor.call("updateStatus", this.props.contact, this.refs.status.value);
+    this.state.opened = false;
+  }
+
+  openDropdown(){
+    this.state.opened = true;
+  }
+
+  closeDropdown(){
+    this.state.opened = false;
+  }
+
   go(){
-    if(this.state.ran){
+    if(this.state.ran||this.state.opened){
       return;
     }
     FlowRouter.go("/contacts/"+this.props.contact._id);
@@ -28,7 +42,20 @@ export default class ContactSingle extends Component {
         <td>{this.props.contact.email}</td>
         <td>{this.props.contact.phone}</td>
         <td>{this.props.contact.newsletter ? "Yes":"No"}</td>
-        <td>{this.props.contact.member ? "Member":"Contact"}</td>
+        {/*<td>{this.props.contact.status //this.props.contact.member ? "Member":"Contact"}</td> */}
+        {checkPermission("admin") ? <td><select
+            ref="status"
+            value={this.props.contact.status}
+            onClick={this.openDropdown.bind(this)}
+            onBlur={this.closeDropdown.bind(this)}
+            onChange={this.changeStatus.bind(this)} >
+          <option value="Crowd">Crowd</option>
+          <option value="Visitor">Visitor</option>
+          <option value="Member">Member</option>
+          <option value="Server">Server</option>
+          <option value="Leader">Leader</option>
+          <option value="Multiplier">Multiplier</option>
+        </select></td> : <td>{this.props.contact.status}</td>}
         {this.props.perm?<td><button className="btn btn-primary" onClick={this.viewTicket.bind(this)}>View Ticket</button></td>:""}
       </tr>
 
