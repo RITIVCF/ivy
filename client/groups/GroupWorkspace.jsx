08277c409fe30c99597df2b6af459ae289b5d0fc
awe6013 <awe6013@rit.edu>
2017-01-23 21:31:57 -0500

    Added leader field to group edit form.

--- client/groups/GroupWorkspace.jsx
+++ client/groups/GroupWorkspace.jsx
@@ -5,6 +5,7 @@ import GroupName from './components/GroupName.jsx';
 import GroupContactControl from './components/GroupContactControl.jsx';
 import GroupUserControl from './components/GroupUserControl.jsx';
 import GroupPermissionControl from './GroupPermissionControl.jsx';
+import SelectUser from '../sharedcomponents/SelectUser.jsx';
 
 export default class GroupsWorkspace extends TrackerReact(React.Component) {
 	constructor(props) {
@@ -22,6 +23,10 @@ export default class GroupsWorkspace extends TrackerReact(React.Component) {
 		// 	contact: false
     // };
   }
+
+	componentDidMount(){
+		Materialize.updateTextFields();
+	}
 
   componentWillUnmount() {
     // this.state.subscription.Churches.stop();
@@ -44,6 +49,18 @@ export default class GroupsWorkspace extends TrackerReact(React.Component) {
 		this.setState({showPerm: !this.state.showPerm});
 	}
 
+	setLeader(user){
+		Meteor.call("setGroupLeader", this.props.group._id, user._id);
+	}
+
+	getLeader(){
+		return Meteor.users.findOne(this.props.group.leader).name;
+	}
+
+	unset(){
+
+	}
+
 	render() {
 		if(!this.props.group){
 			return <div>
@@ -52,7 +69,6 @@ export default class GroupsWorkspace extends TrackerReact(React.Component) {
 			</div>
 		}
 
-
 		return (
 		<div className="row">
 			<div className="col s12">
@@ -60,6 +76,19 @@ export default class GroupsWorkspace extends TrackerReact(React.Component) {
 				{/*<input type="checkbox" ref="admin" value={this.props.group.admingroup} onChange={this.toggleAdmin.bind(this)} />*/}
 				{/*this.props.group.admingroup ? <p><i>This is an admin group.</></p>:<div></div>*/}
 				{this.props.group._id!="admin"?<GroupPermissionControl group={this.props.group} />:false}
+				{(this.props.group.type=="Small Group"||this.props.group.type=="Team")&&
+					<div className="row">
+						<div className="col s8">
+							<p>Set leader:</p>
+							<SelectUser
+								initialValue={this.getLeader()}
+								id="leaderselect"
+								keepName={true}
+								unset={this.unset.bind(this)}
+								updateUser={this.setLeader.bind(this)}
+								ref="leader" />
+						</div>
+					</div>}
 				<GroupUserControl group={this.props.group} />
 			</div>
 		</div>
