cbbaeb7e678d7b0fc23dc2a498dc2cfd2d067912
awe6013 <awe6013@rit.edu>
2017-04-23 15:21:56 -0400

    Removed contact and attendance backup cron job. Changed the cron job times to be 4:00am. Changed order where it updates the funnel statuses first, then takes a snapshot.

--- server/methods.js
+++ server/methods.js
@@ -237,6 +237,90 @@ Meteor.methods({
   testCreation(){
     console.log(Accounts.createUser({email: "testemail@email.com"}));
   },
+  updateContactFunnelStatus(uid){
+    var intervl = 7; //In days
+    var period = 4; //In intervls
+    var threshold = 2; //Integer # of intervals
+    var endDate = new Date();
+    var startDate = new Date();
+    //startDate.setDate(startDate.getDate() - period);
+    startDate = new moment(startDate.toISOString()).subtract(intervl,"days")._d
+    //var eventsPerInterval = {};
+    var eventsPerInterval = [];
+    for (var c = 0; c < period; c++) { //Sets up an object with key:number of intervals ago, and value:a list of events that went on in that interval.
+      var events = [];
+      startDate = new moment(endDate.toISOString()).subtract(intervl,"days")._d;
+      //eventsPerInterval[(c+1)] = Events.find({
+      var eventsFound = Events.find({
+        start:{
+          $gte : startDate,//startDate.setDate(startDate.getDate() - intervl),
+          $lt : endDate
+        },
+        published: true
+      }).fetch();
+      if(eventsFound.length==0){
+        endDate = startDate;
+        c--;
+        continue;
+      }
+      eventsFound.forEach((event)=>{
+        events.push(event._id);
+      });
+      eventsPerInterval.push(events);
+      //endDate.setDate(endDate.getDate() - intervl);
+      // console.log("endDate: ", endDate);
+      // console.log("startDate: ", startDate);
+      endDate = startDate;//new moment(endDate.toISOString()).subtract(intervl,"days")._d
+      //console.log("New endDate: ", endDate);
+    }
+    var mults = Groups.findOne("multipliers").users;
+    //console.log("Users: ", Meteor.users.find({deleted: {$ne: true}}).fetch());
+    //for (var i in Meteor.users.find({deleted: {$ne: true}}).fetch()) {
+    //Meteor.users.find({deleted: {$ne: true}}).fetch().map((user)=>{
+    let user = Meteor.users.findOne(uid);
+      //var uid = user._id;
+      //console.log(uid, user.name);
+      // console.log("Is Leader: ", Groups.find({leader: uid}).fetch());
+      //if (uid in mults) {
+      if(Groups.find({_id: "multipliers", users: uid}).fetch().length>0){
+        // console.log(user.name, " Is multiplier");
+        Meteor.users.update({_id : uid}, {$set : {status : "Multiplier"}});
+      } else if (Groups.find({leader : uid}).fetch().length > 0) {
+        // console.log(user.name, " Is leader");
+        Meteor.users.update({_id : uid}, {$set : {status : "Leader"}});
+      } else if (Groups.find({type : "Team", users : uid}).fetch().length > 0) {
+        //console.log(user.name, " Is server");
+        Meteor.users.update({_id : uid}, {$set : {status : "Server"}});
+      } else if (user.member) {
+        // console.log(user.name, " Is member");
+        Meteor.users.update({_id : uid}, {$set : {status : "Member"}});
+      } else {
+        // console.log(user.name, " Is visitor or crowd");
+        var count = 0;
+        //console.log(eventsPerInterval);
+        eventsPerInterval.forEach((intvl)=>{
+        //  console.log(intvl);
+          if(Events.find({_id: {$in: intvl}, "attendees._id": uid}).fetch().length>0){
+            count++
+          }
+        });
+
+        // for (var ls in eventsPerInterval) {
+        // 	if (ls.find({attendees : uid}).fetch().length > 0) {
+        // 		count++;
+        // 	}
+        // }
+        if (count>=threshold) {
+          // console.log(user.name, " Is visitor");
+          Meteor.users.update({_id : uid}, {$set : {status : "Visitor"}});
+        } else {
+          // console.log(user.name, " Is crowd");
+          Meteor.users.update({_id : uid}, {$set : {status : "Crowd"}});
+        }
+      }
+    //  console.log(user.name, " end.");
+  //   });
+  },
   migrateDatabase(){
     //var userMeteor.users = Meteor.users.find({user: true}).fetch();
     var notUserContacts = Contacts.find({user: {$ne: true}}).fetch();
