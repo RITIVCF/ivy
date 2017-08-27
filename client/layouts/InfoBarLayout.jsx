e66215dbeafa0b7b74f273dd4e87063b96c78c53
awe6013 <awe6013@rit.edu>
2017-08-27 17:53:49 -0400

    Removed import from a deleted file.

--- client/layouts/InfoBarLayout.jsx
+++ client/layouts/InfoBarLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SignInButtonWrapper from '/client/user/SignInButtonWrapper.jsx';
-import NavBar from './NavBar.jsx';
+//import NavBar from './NavBar.jsx';
 import InfoBarMain from './InfoBarMain.jsx';
 
 export var InfoBarLayout = ({header,content}) => (   // export const MainLayout
