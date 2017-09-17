0a2429fae4a89f373b3f28d272d175d8a05f871e
awe6013 <awe6013@rit.edu>
2017-09-17 18:15:57 -0400

    Installed NPM module. Started playing with different dialog solutions on the Dashboard.

--- client/Dashboard.jsx
+++ client/Dashboard.jsx
@@ -6,22 +6,49 @@ import MyEvents from './widgets/MyEvents.jsx';
 import MyTickets from './widgets/MyTickets.jsx';
 import MyGroups from './widgets/MyGroups.jsx';
 import SafariWarning from './SafariWarning.jsx';
+import Dialog from 'material-ui/Dialog';
 
 export default class DashboardWrapper extends TrackerReact(React.Component) {
 	constructor(){
 		super();
 		this.state = {
+			modalOpen: false,
 			subscription: {
 				user: Meteor.subscribe("userSelf")
 			}
 		}
+
+		this.openModal = this.openModal.bind(this);
+		this.closeModal = this.closeModal.bind(this);
+	}
+
+	openModal(){
+		this.setState({modalOpen: true});
+	}
+
+	closeModal(){
+		this.setState({modalOpen: false});
 	}
 
 	render() {
 		setDocumentTitle("Dashboard");
+		const actions = [
+			<Button onClick={this.closeModal}>Close Modal</Button>
+		];
 		return (
 				<div className="row">
 					<div className="col s12 m6">
+						<Card>
+							<Button onClick={this.openModal}>Open Modal</Button>
+							<Dialog
+								title="Test Title"
+								modal={false}
+								actions={actions}
+								open={this.state.modalOpen}
+							>
+								Some Content
+							</Dialog>
+						</Card>
 						<SafariWarning />
 						<MySchedule />
 						<MyTickets />
